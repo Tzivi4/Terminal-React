b1ef8923c440c2ecbad447a931cd6b5ab1e1bdca
Tzivi <stavgamer00@gmail.com>
2020-08-18 10:59:31 -0300

    .

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -6,6 +6,12 @@ const NotFound = () => (
     <div className="text-center">
         <h1 className="Error_Text">Error: 404 Page Not Found</h1>
         <img src={NotFoundImg} alt="404 NotFound" className="Error_Image" />
+        <Link to="/inicio">
+            <button>
+                Inicio
+                <i class="fas fa-arrow-right"></i>
+            </button>
+        </Link>
     </div>
 )
 
